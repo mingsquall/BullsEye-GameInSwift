ce47561a926b519fd5af6ddd754de8fd70eed8a2
YuanMing Liu <774166816@qq.com>
2016-10-11 15:40:14 +0800

    Version 1.0

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,45 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Patientman on 16/9/22.
+//  Copyright © 2016年 mingSquall. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    @IBOutlet weak var webView: UIWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //First it finds the BullsEye.html file in the application bundle, then loads it into an NSData object, and finally it asks the web view to show the contents of this data object.
+        if let htmlFile = NSBundle.mainBundle().pathForResource("BullsEye", ofType: "html") {
+            if let htmlData = NSData(contentsOfFile: htmlFile) {
+                let baseURL = NSURL(fileURLWithPath: NSBundle.mainBundle().bundlePath)
+                webView.loadData(htmlData, MIMEType: "text/html", textEncodingName: "UTF-8", baseURL: baseURL)
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func close() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BullsEye/ViewController.swift
@@ -0,0 +1,133 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Patientman on 16/2/11.
+//  Copyright © 2016年 mingSquall. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
+    
+    //Set variable to calculate the Slider's value
+    var currentValue = 0
+    var targetValue = 0
+    var score = 0
+    var round = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let thumbImageNormal = UIImage(named: "SliderThumb-Normal")
+        slider.setThumbImage(thumbImageNormal, forState: .Normal)
+        
+        let thumbImageHighLighted = UIImage(named: "SliderThumb-Highlighted")
+        slider.setThumbImage(thumbImageHighLighted, forState: .Highlighted)
+        
+        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+        
+        if let trackLeftImage = UIImage(named: "SliderTrackLeft") {
+            let trackLeftResizable = trackLeftImage.resizableImageWithCapInsets(insets)
+            slider.setMinimumTrackImage(trackLeftResizable, forState: .Normal)
+        }
+        
+        if let trackRightImage = UIImage(named: "SliderTrackRight") {
+            let trackRightResizable = trackRightImage.resizableImageWithCapInsets(insets)
+            slider.setMaximumTrackImage(trackRightResizable, forState: .Normal)
+            
+        }
+        
+        startNewGame()
+        updateLabels()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+    }
+    
+    func startNewGame() {
+        score = 0
+        round = 0
+        startNewRound()
+    }
+    
+    func updateLabels() {
+        //"\(targetValue)" Equal to String(targetValue)
+        targetLabel.text = "\(targetValue)"
+        scoreLabel.text = "\(score)"
+        roundLabel.text = String(round)
+    }
+    
+    
+    @IBAction func showAlert() {
+        //set the difference value to show the difference between Targers and Slider
+        let difference = abs(currentValue - targetValue)
+        var points = 100 - difference
+
+        round += 1
+        //show the alert
+//        let message = "The value of the slider is: \(currentValue)"
+//                  + "\nThe target value is : \(targetValue)"
+//                  + "\nThe difference is : \(difference)"
+        let title:String
+        if difference == 0 {
+            title = "Perfect!"
+            points += 100
+        } else if difference < 5 {
+            title = "You almost had it!"
+            if difference == 1 {
+                points += 50
+            }
+        } else if difference < 10 {
+            title = "Pretty good!"
+        } else {
+            title = "Not even close.."
+        }
+        
+        score += points
+        let message = "Your score \(points) points"
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        let action = UIAlertAction(title: "OK", style: .Default, handler: { action in
+                                                                            self.startNewRound()
+                                                                            self.updateLabels()
+                                                                          })
+        alert.addAction(action)
+        presentViewController(alert, animated: true, completion: nil)
+   
+    }
+
+    @IBAction func sliderMoved(slider: UISlider) {
+        //将Float转换为Int.Rounding off the slider's decimals
+        currentValue = lroundf(slider.value)
+    }
+    
+    @IBAction func startOver() {
+        startNewGame()
+        updateLabels()
+        
+        let transition = CATransition()
+        transition.type = kCATransitionFade
+        transition.duration = 1
+        transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+        view.layer.addAnimation(transition, forKey: nil)
+        
+    }
+
+
+}
+
